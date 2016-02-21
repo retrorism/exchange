8a1ecc15e56a97225a73277c470ae48d7ef638bb
Willem Prins <willem@somtijds.nl>
2016-02-21 16:19:07 +0100

    Create composer.js
    Listing this fork's 'version' and author credits

---
+++ composer.js
@@ -0,0 +1,25 @@
+
+{
+    "name": "retrorism/exchange",
+    "description": "A WordPress theme for Tandem. Built with Foundation 6, based upon Jeremy Englert's JointsWP http://jointswp.com"
+    "authors"     : [
+      {
+        "name": "Jeremy Englert",
+        "homepage": "http://jointswp.com"
+      },
+      {
+        "name"    : "ZURB",
+        "homepage": "https://foundation.zurb.com
+      {
+        "name"    : "Willem Prins",
+        "email"   : "willem@somtijds.nl",
+        "homepage": "http://www.somtijds.nl/"
+}
+    ],
+    "version": "0.1",
+    "type": "wordpress-theme",
+    "dist": {
+        "url": "https://github.com/retrorism/exchange-plugin.git",
+        "type": "git"
+    }
+}
