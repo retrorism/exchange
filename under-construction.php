f3c9ae1e28277a47b001c83aeca2783c78c45d13
Willem Prins | SOMTIJDS <willem@somtijds.nl>
2016-04-26 12:27:52 +0200

    add relative icon url

--- under-construction.php
+++ under-construction.php
@@ -3,6 +3,8 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<?php $root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/'; ?>
+
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,13 +12,13 @@
 		<meta property="og:title" content="Tandem Exchange (under construction)" />
 		<meta property="og:description" content="This is where we build the new Tandem Website" />
 		<meta property="og:url" content="http://www.tandemexchange.eu" />
-		<meta property="og:image" content="http://www.tandemexchange.eu/assets/images/ampersand_white_bg.png" />
+		<meta property="og:image" content="<?php echo $root; ?>site/wp-content/themes/exchange/assets/images/ampersand_white_bg.png" />
     	<meta name="description" content="Come back soon for the new website for Tandem Exchange">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/exchange.css">
 
-	</head>
+	</head
 	<body class="noscroll">
    	 	<!--[if lt IE 7]>
         	<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
