a6978772dd2a21184f3b6ef64b9812ad17cafa2c
Willem Prins | SOMTIJDS <willem@somtijds.nl>
2016-05-26 12:28:28 +0200

    Small styling and code fix for focus images

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,16 +11,16 @@ and Foundation play nice together.
  * @TODO : only do this when the header image crop size is *not* available.
  *
  */
-var getFocusTranslate = function( header_img_container ) {
-	header_img = header_img_container.childNodes[1],
-	header_img_data = header_img_container.dataset;
-	if ( ! header_img_data ) {
+var getFocusTranslate = function( img_container ) {
+	img = img_container.childNodes[1],
+	img_data = img_container.dataset;
+	if ( ! img_data ) {
 		return false;
 	}
-	var h = header_img.offsetHeight,
-	container_h =  header_img_container.offsetHeight,
-	px_translate = ( header_img_data.focus_h * h ) - ( container_h / 2 ),
-	max_translate = ( ( container_h / 2 ) / h ) * 100,
+	var h = img.offsetHeight,
+	container_h =  img_container.offsetHeight,
+	px_translate = ( img_data.focus_h * h ) - ( container_h / 2 ),
+	max_translate = ( ( h - container_h ) / h ) * 100,
 	translate = ( px_translate / h ) * 100;
 	// If the center of the container is below the focus point, don't move.
 	// Or: if the translation is downwards, don't move (we're working from top).
@@ -28,12 +28,12 @@ var getFocusTranslate = function( header_img_container ) {
 		console.log( px_translate );
 		return false;
 	}
-	// console.log( 'image height: ' + h );
-	// console.log( 'container center ' + ( container_h / 2 ) );
-	// console.log( 'focus point on ' + ( header_img_data.focus_h * h ) );
-	// console.log( 'number of pixels to move: ' + px_translate );
-	// console.log( 'number of percents to move: ' + translate );
-	// console.log( 'max percent to move: ' + max_translate );
+	console.log( 'image height: ' + h );
+	console.log( 'container center ' + ( container_h / 2 ) );
+	console.log( 'focus point on ' + ( img_data.focus_h * h ) );
+	console.log( 'number of pixels to move: ' + px_translate );
+	console.log( 'number of percents to move: ' + translate );
+	console.log( 'max percent to move: ' + max_translate );
 	if ( translate > max_translate ) {
 		return max_translate;
 	} else {
@@ -43,16 +43,16 @@ var getFocusTranslate = function( header_img_container ) {
 
 jQuery(document).ready(function() {
 
-	var header_img_container = document.querySelector('.story__header__image'),
+	var header_img_container = document.querySelector('.focus'),
 	header_img = header_img_container.childNodes[1],
 	translate = getFocusTranslate( header_img_container );
 	if ( translate ) {
-		header_img.setAttribute( 'style', 'transform: translateY(-' + translate + '%');
+		header_img.setAttribute( 'style', 'transform: translateY(-' + translate + '%); opacity: 1; visibility: visible;');
 	}
 
 
-    // Remove empty P tags created by WP inside of Accordion and Orbit
-    jQuery('.accordion p:empty, .orbit p:empty').remove();
+	// Remove empty P tags created by WP inside of Accordion and Orbit
+	jQuery('.accordion p:empty, .orbit p:empty').remove();
 
 	 // Makes sure last grid item floats left
 	jQuery('.archive-grid .columns').last().addClass( 'end' );
