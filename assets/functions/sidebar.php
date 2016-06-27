97ba18d66b740447959463a12d807c68a2311155
Willem Prins | SOMTIJDS <willem@somtijds.nl>
2016-06-27 12:37:51 +0200

    Footer names and descriptions fixed

--- assets/functions/sidebar.php
+++ assets/functions/sidebar.php
@@ -4,7 +4,7 @@ function exchange_register_sidebars() {
 	register_sidebar(array(
 		'id' => 'footer-1',
 		'name' => __('Footer 1', 'exchange'),
-		'description' => __('The first (primary) sidebar.', 'exchange'),
+		'description' => __('Footer widget', 'exchange'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget' => '</div>',
 		'before_title' => '<h4 class="widget__title">',
@@ -13,8 +13,8 @@ function exchange_register_sidebars() {
 
 	register_sidebar(array(
 		'id' => 'footer-2',
-		'name' => __('Footer 21', 'exchange'),
-		'description' => __('The first (primary) sidebar.', 'exchange'),
+		'name' => __('Footer 2', 'exchange'),
+		'description' => __('Footer widget', 'exchange'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget' => '</div>',
 		'before_title' => '<h4 class="widget__title">',
@@ -24,7 +24,7 @@ function exchange_register_sidebars() {
 	register_sidebar(array(
 		'id' => 'footer-3',
 		'name' => __('Footer 3', 'exchange'),
-		'description' => __('The first (primary) sidebar.', 'exchange'),
+		'description' => __('Footer widget', 'exchange'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget' => '</div>',
 		'before_title' => '<h4 class="widget__title">',
@@ -34,7 +34,7 @@ function exchange_register_sidebars() {
 	register_sidebar(array(
 		'id' => 'footer-4',
 		'name' => __('Footer 4', 'exchange'),
-		'description' => __('The first (primary) sidebar.', 'exchange'),
+		'description' => __('Footer widget', 'exchange'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget' => '</div>',
 		'before_title' => '<h4 class="widget__title">',
