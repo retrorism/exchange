6ea875db039535da29cf42e107f2242e411964bd
Willem Prins | SOMTIJDS <willem@somtijds.nl>
2016-08-15 12:30:18 +0200

    add failsave for sharing template option

--- parts/content-sharing-footer.php
+++ parts/content-sharing-footer.php
@@ -18,9 +18,11 @@
 		<div class="button-wrapper">
 			<?php
 				$story_form_page = get_option( 'options_story_update_form_page' );
-				$exchange_form = BaseController::exchange_factory( $story_form_page );
-				if ( $exchange_form instanceof Exchange ) {
-					echo exchange_create_link( $exchange_form, false, 'button--large button--share-cta' ) . __('Share your story','exchange') . '</a>';
+				if ( ! empty( $story_form_page ) ) {
+					$exchange_form = BaseController::exchange_factory( $story_form_page );
+					if ( $exchange_form instanceof Exchange ) {
+						echo exchange_create_link( $exchange_form, false, 'button--large button--share-cta' ) . __('Share your story','exchange') . '</a>';
+					}
 				}
 			?>
 		</div>
