f44f023b8b96787c98214e1fec06ec596f676ebc
Willem Prins | SOMTIJDS <willem@somtijds.nl>
2016-09-08 22:01:17 +0200

    Small style fixes

--- assets/functions/admin.php
+++ assets/functions/admin.php
@@ -4,12 +4,12 @@
 /************* DASHBOARD WIDGETS *****************/
 // Disable default dashboard widgets
 function disable_default_dashboard_widgets() {
-	// Remove_meta_box('dashboard_right_now', 'dashboard', 'core');    // Right Now Widget
+	remove_meta_box('dashboard_right_now', 'dashboard', 'core');    // Right Now Widget
 	remove_meta_box('dashboard_recent_comments', 'dashboard', 'core'); // Comments Widget
 	remove_meta_box('dashboard_incoming_links', 'dashboard', 'core');  // Incoming Links Widget
 	remove_meta_box('dashboard_plugins', 'dashboard', 'core');         // Plugins Widget
 
-	// Remove_meta_box('dashboard_quick_press', 'dashboard', 'core');  // Quick Press Widget
+	remove_meta_box('dashboard_quick_press', 'dashboard', 'core');  // Quick Press Widget
 	remove_meta_box('dashboard_recent_drafts', 'dashboard', 'core');   // Recent Drafts Widget
 	remove_meta_box('dashboard_primary', 'dashboard', 'core');         //
 	remove_meta_box('dashboard_secondary', 'dashboard', 'core');       //
